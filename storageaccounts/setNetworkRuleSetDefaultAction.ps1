3881c0b63f1d1893bae4c8cebc2b8fe4a592303d
abhishek choudharry <abhishek.choudharry@gmail.com>
2024-07-24 20:18:15 +0530

    Updated the documentation

--- storageaccounts/setNetworkRuleSetDefaultAction.ps1
+++ storageaccounts/setNetworkRuleSetDefaultAction.ps1
@@ -1,3 +1,26 @@
+<#
+.SYNOPSIS
+Sets the network ACLs default action to "Deny" for a list of Azure Storage Accounts.
+
+.DESCRIPTION
+This PowerShell script iterates through a predefined list of Azure Storage Accounts and sets the network ACLs default action to "Deny" for each. It uses the Azure CLI to update the configuration of each Storage Account, ensuring that only explicitly allowed traffic can access the storage account. This script enhances the security posture by restricting unintended network access.
+
+.PARAMETER accounts
+An array of hashtable entries, each containing the 'ResourceGroup' and 'StorageAccount' name. This parameter is mandatory and must be provided when calling the function. The script will iterate through each entry in this array to update the network ACLs default action for the specified Storage Accounts.
+
+.EXAMPLE
+.\Set-NetworkAclsDefaultAction.ps1
+
+This example demonstrates how to run the script, which sets the network ACLs default action to "Deny" for the Storage Accounts defined in the $storageAccounts variable. Ensure that the $storageAccounts variable is populated with the correct Storage Account names and their corresponding resource groups before running the script.
+
+.NOTES
+- Ensure you have the Azure CLI installed and are authenticated to Azure with 'az login' before running this script.
+- The script checks if each Storage Account exists before attempting to update its configuration. If a Storage Account does not exist, a message is displayed, and the script continues to the next Storage Account in the list.
+- It's recommended to review the current network ACLs configuration for each Storage Account before running this script to avoid unintentional access restrictions.
+- The script sets the networkRuleSet.defaultAction to "Deny" and bypasses AzureServices, allowing Azure services to still access the storage account. Adjust the bypass settings as necessary for your environment.
+
+#>
+
 # List of Storage Accounts with their Resource Groups
 $storageAccounts = @(
     @{ ResourceGroup = "RG-Test"; StorageAccount = "test123abc" }
