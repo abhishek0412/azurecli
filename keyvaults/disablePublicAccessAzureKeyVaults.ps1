ecd26cd8213068bde195430f6297cfee8e6d3ecd
abhishek choudharry <abhishek.choudharry@gmail.com>
2024-07-25 13:46:02 +0530

    Updated the scripts to check if access is already disabled.

--- keyvaults/disablePublicAccessAzureKeyVaults.ps1
+++ keyvaults/disablePublicAccessAzureKeyVaults.ps1
@@ -3,13 +3,13 @@
 Disables public network access for a list of Azure Key Vaults.
 
 .DESCRIPTION
-This PowerShell script iterates through a predefined list of Azure Key Vaults and disables public network access for each. It uses the Azure CLI to update the network ACLs of each Key Vault, setting the default action to deny access from public networks. The script checks if each Key Vault exists before attempting to update it and preserves any existing IP and virtual network rules to ensure that specific allowed networks maintain access.
+This PowerShell script iterates through a predefined list of Azure Key Vaults and disables public network access for each. It uses the Azure CLI to update the network ACLs of each Key Vault, setting the default action to deny access from public networks. The script checks if each Key Vault exists before attempting to update it.
 
 .PARAMETER keyVaults
 An array of hashtable entries, each containing the 'Name' and 'ResourceGroup' of the Key Vault. This parameter is mandatory and must be provided when calling the function. The script will iterate through each entry in this array to disable public network access for the specified Key Vaults.
 
 .EXAMPLE
-.\disablePublicAccessAzureKeyVaults.ps1
+.\DisablePublicAccessAzureKeyVaults.ps1
 
 This example demonstrates how to run the script, which disables public network access for the Key Vaults defined in the $keyVaults variable. Ensure that the $keyVaults variable is populated with the correct Key Vault names and their corresponding resource groups before running the script.
 
@@ -22,7 +22,7 @@ This example demonstrates how to run the script, which disables public network a
 
 # List of Key Vault names and their corresponding resource groups
 $keyVaults = @(
-    @{ Name = "akv1111"; ResourceGroup = "RG_Test" }
+    @{ Name = "<keyvault-name>"; ResourceGroup = "<resourecegroup-name>" }
 )
 
 # Function to disable public network access for a list of Key Vaults using Azure CLI
@@ -40,29 +40,17 @@ function Disable-PublicNetworkAccess {
         $existingVault = az keyvault show --name $vaultName --resource-group $resourceGroupName --query 'name' -o tsv
 
         if ($existingVault) {
-            # Get the current network rules
-            $networkRules = az keyvault network-rule list --name $vaultName --resource-group $resourceGroupName --output json | ConvertFrom-Json
-			# Update the network ACLs to disable public access
-            
-			az keyvault update --name $vaultName --resource-group $resourceGroupName --default-action Deny
-            Write-Output "Disabled public network access for Key Vault: $vaultName in Resource Group: $resourceGroupName"
-			
-            if ($networkRules) {
-                # Reapply existing IP rules and VNet rules if they exist
-                if ($networkRules.ipRules) {
-                    foreach ($ipRule in $networkRules.ipRules) {
-                        az keyvault network-rule add --name $vaultName --resource-group $resourceGroupName --ip-address $ipRule.value
-						Write-Output "Adding ip rule for Key Vault: $vaultName in Resource Group: $resourceGroupName ip: $ipRule.value"
-                    }
-                }
+            # Check if public network access is enabled
+            $publicNetworkAccess = az keyvault show --name $vaultName --resource-group $resourceGroupName --query 'properties.networkAcls.defaultAction' -o json | ConvertFrom-Json
 
-                if ($networkRules.virtualNetworkRules) {
-                    foreach ($vnetRule in $networkRules.virtualNetworkRules) {
-                        az keyvault network-rule add --name $vaultName --resource-group $resourceGroupName --vnet-name $vnetRule.id.split('/')[-3] --subnet $vnetRule.id.split('/')[-1]
-						Write-Output "Adding vnet for Key Vault: $vaultName in Resource Group: $resourceGroupName vnet-name: $vnetRule.id"
-					}
-                }    
-            } 
+            if ($publicNetworkAccess -ne "Deny") {
+                # Update the network ACLs to disable public access
+                az keyvault update --name $vaultName --resource-group $resourceGroupName --default-action Deny
+                Write-Output "Disabled public network access for Key Vault: $vaultName in Resource Group: $resourceGroupName"
+
+            } else {
+                Write-Output "Public network access is already disabled for Key Vault: $vaultName in Resource Group: $resourceGroupName"
+            }
         } else {
             Write-Output "Key Vault: $vaultName not found in Resource Group: $resourceGroupName"
         }
